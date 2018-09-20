1189186a84f8b4617326059bec31eff996aef2b7
George Charles Chang <31757304+georgec12104531@users.noreply.github.com>
2018-09-20 13:25:47 -0700

    Create intro_javascript_hw.js

---
+++ W5D4/intro_javascript_hw.js
@@ -0,0 +1,19 @@
+
+// madLib
+function madLib(verb, adj, noun) {
+  return `I want to ${verb} ${adj} ${noun} right now!`
+}
+
+// isSubString
+
+function isSubString(string, substring) {
+  if (string.includes(substring)) {
+    return true;
+  }
+  return false;
+}
+
+// fizzbuzz
+
+
+function fizzBuzz()
